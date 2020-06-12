49eea238c2b3b58afea5360795df4abfad48c140
Apostrov <amir.subaev@gmail.com>
2020-06-12 16:44:29 +0300

    Add navmesh generator

---
+++ Assets/Scripts/AgentMove.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentMove : MonoBehaviour
+{
+    public Transform target;
+    void Start()
+    {
+        var agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(target.position);
+    }
+    
+}
---
+++ Assets/Scripts/FixAgent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FixAgent : MonoBehaviour
+{
+    void Start()
+    {
+        var agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+   
+}
