77b980cfd6fe326dc01baa90c881732ec9f1ac92
Apostrov <amir.subaev@gmail.com>
2020-06-14 22:09:39 +0300

    Prepare for brush

---
+++ Assets/Scripts/BrushAgent.cs
@@ -0,0 +1,9 @@
+﻿public class BrushAgent : SimpleAgent
+{
+    protected override void StartWorkAnimation()
+    {
+        _currentWorkAnimation = "brush";
+        animator.SetBool(_currentWorkAnimation, true);
+        workAudio.Play();
+    }
+}
