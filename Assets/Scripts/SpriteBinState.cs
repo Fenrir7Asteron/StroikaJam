9a31ddf6f1cae532b3b1db51325d616aaafecee1
しずえ <cauchymoon@mail.ru>
2020-06-14 13:26:29 +0300

    Signs that show completion work and show stars.

---
+++ Assets/Scripts/SpriteBinState.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SpriteBinState : MonoBehaviour
+{
+    Image image;
+
+    public bool on = false;
+
+    public Sprite deactivated;
+    public Sprite activated;
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (on)
+            image.sprite = activated;
+        else
+            image.sprite = deactivated;
+    }
+}
