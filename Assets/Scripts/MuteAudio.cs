2eeea62e09198734a17ad1ecf6d436af3677f44d
Apostrov <amir.subaev@gmail.com>
2020-06-14 21:55:18 +0300

    Add sound button

---
+++ Assets/Scripts/MuteAudio.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MuteAudio : MonoBehaviour
+{
+    public GameObject soundButton;
+    public GameObject noSoundButton;
+
+    public void MuteAudioAndChangeButton()
+    {
+        AudioListener.volume = 0;
+        soundButton.SetActive(false);
+        noSoundButton.SetActive(true);
+    }
+
+    public void UnMuteAudioAndChangeButton()
+    {
+        AudioListener.volume = 1;
+        noSoundButton.SetActive(false);
+        soundButton.SetActive(true);
+    }
+}
